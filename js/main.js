1dc3b89370c91e62061830a03dcc4f7e4189a372
Robert Feie <rfeie@hotmail.com>
2017-09-26 19:51:57 -0500

    hey second commit

--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
-// log to doc function
-// main function
-// async
-//
-
 setupFlightInfoDependentData()
-Number.parseInt();
 
 function display () {
   const container = document.querySelector('.content')
@@ -41,40 +35,79 @@ const error = (text) => log('ERROR:', JSON.stringify(text))
   const calls = callFlightAPIs(handlers.flight, handlers.weather, handlers.api)
   const flightInfo = await calls.next().value
   if (flightInfo) {
+    log('Starting the weather call')
     const weatherProm = calls.next().value
+    log('Starting the airline api call')
     const ASAApiProm = calls.next().value
+
+    log('waiting for weather')
     const weather = await weatherProm
+    log('waiting for ASA API')
     const ASAApi = await ASAApiProm
+    log('finished!')
   }
 }
 
 function *callFlightAPIs(FIHandler, ASAInfoHandler, WeatherHandler) {
-  let num
-  yield apiCall()
+  let destination
+  let flightNum
+  yield getFlightInfo()
     .then(res => {
       FIHandler.success(res)
-      num = res
+      destination = res.flightInfo.destination
+      flightNum = res.flightInfo.flightNum
       return true
     })
     .catch(e => false)
-  yield apiCall().catch(e => false)
+  yield getWeatherData(destination).catch(e => false)
     .then(res => {
-      WeatherHandler.success(res + ' ' + num)
+      WeatherHandler.success(JSON.stringify(res) + ' ' + destination)
       return true
     })
     .catch(e => false)
-  yield apiCall().catch(e => false)
+  yield getAPIData(flightNum).catch(e => false)
     .then(res => {
-      ASAInfoHandler.success(res + ' ' + num)
+      ASAInfoHandler.success(JSON.stringify(res) + ' ' + flightNum)
       return true
     })
     .catch(e => false)
 }
 
+function getAPIData(flightNum) {
+  const data = {
+    flightNum: flightNum,
+    gate: "G11",
+    meals: ['steak', 'hotdogs']
+  }
 
-function apiCall() {
+  return apiCall(data)
+}
+
+function getWeatherData(dest) {
+  const data = {
+    temperature: 75,
+    destination: dest,
+    description: "It's cloudy"
+  }
+
+  return apiCall(data)
+}
+
+function getFlightInfo() {
+  const data = {
+    flightInfo: {
+      flightNum: "RGF827",
+      tail: "RGF000",
+      origin: "ORD",
+      destination: "KFL"
+    }
+  }
+  return apiCall(data)
+}
+
+function apiCall(data) {
   return new Promise((resolve, reject) => {
     const timeout = Math.floor(Math.random()*1000)
-    setTimeout(() => resolve(timeout), timeout)
+    setTimeout(() => resolve(data), timeout)
   })
 }
